c0d467df80dc8f77a31a3da759c2b0236132c98d
Tobias Batch <tobias@neontribe.co.uk>
2023-03-08 10:07:42 +0000

    feat: added API call to WHOAMI in next app

--- nextjs/src/pages/api/auth/whoami.ts
+++ nextjs/src/pages/api/auth/whoami.ts
@@ -1,13 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type {NextApiRequest, NextApiResponse} from 'next'
+import {WHO_AM_I_ROUTE} from "@/config";
 
 type Data = {
   name: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const bcAppSession = "CURRENTLY UNKNOWN"
+  const options = {
+    method: 'get',
+    headers: new Headers({
+        'Authorization': `Bearer ${bcAppSession}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }),
+  }
+
+  const response = await fetch(WHO_AM_I_ROUTE, options)
+  const content = await response.text()
+
+  let data = {}
+  if (content) {
+    data = {}
+  } else {
+    data = {}
+  }
+
+  res.status(response.status).json({name: ""})
 }
